5d81c09569ed6b3d505d6d33534fb4e2b26999a9
Matty <hello@matty.co.za>
2014-02-05 22:09:40 +0200

    Adds logic to prepare to filter the query, to include GivenGain data.

--- classes/class-givengain-frontend.php
+++ classes/class-givengain-frontend.php
@@ -23,6 +23,14 @@ final class Givengain_Frontend {
 	public function __construct ( $file, $api_obj ) {
 		$this->_file = $file;
 		$this->api = $api_obj;
+
+		add_action( 'pre_get_posts', array( $this, 'prepare_output_object' ) );
 	} // End __construct()
+
+	public function prepare_output_object ( $query ) {
+		if ( ! $query->is_main_query() ) return;
+
+		// TODO
+	} // End prepare_output_object()
 } // End Class
 ?>
